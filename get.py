0ae6ce402d6fb8500bba127b7bea5c691d0ef2d9
PoojithXYZ <poojithreddytalla@gmail.com>
2024-08-11 12:38:18 +0530

    yo

--- get.py
+++ get.py
@@ -8,3 +8,10 @@ def read_problems(file_name):
                 max_score = int(lines[1].split(":")[1].strip())
                 problems.append((problem_name, max_score))
     return problems
+
+def make_todo_list(problems = "file_chai.txt", done_Qs = "file_poo.txt"):
+    problems_list = read_problems(problems)
+    done_list = read_problems(done_Qs)
+    todo_list = [problem for problem in problems_list if problem not in done_list]
+    return todo_list
+

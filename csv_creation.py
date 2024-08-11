edf31abfeab652732ecd6177d688bc9e6dcf27ba
PoojithXYZ <poojithreddytalla@gmail.com>
2024-08-11 17:54:31 +0530

    yo

--- csv_creation.py
+++ csv_creation.py
@@ -1,12 +1,13 @@
 import csv
 
+def rewrite_name(problem_name):
+    return problem_name.replace(" ", "-").replace("'", "")
+ 
 def make_si_links(problem_name, is_basic = True):
-    def rewrite_name(problem_name = problem_name):
-        return problem_name.replace(" ", "-").replace("'", "")
     si_link = f"https://hive.smartinterviews.in/contests/smart-interviews-{"basic" if is_basic == True else "primary"}/problems/{rewrite_name(problem_name)}?page=0&pageSize=100"
     return si_link
 
-def read_problems(file_name):
+def read_problems(file_name, is_basic = True):
     problems = []
     with open(file_name, 'r') as file:
         for block in file.read().split("try again"):
@@ -14,18 +15,20 @@ def read_problems(file_name):
             if lines:
                 problem_name = lines[0]
                 max_score = int(lines[1].split(":")[1].strip())
-                problems.append((problem_name, max_score, make_si_links(problem_name)))
+                problems.append((problem_name, max_score, make_si_links(problem_name, is_basic)))
     return problems
 
-def make_todo_list(problems = "chai_links.txt", done_Qs = "poo_links.txt"):
-    problems_list = read_problems(problems)
-    done_list = read_problems(done_Qs)
+def make_todo_list(problems = "chai_links.txt", done_Qs = "poo_links.txt", is_basic = True):
+    problems_list = read_problems(problems, is_basic)
+    done_list = read_problems(done_Qs, is_basic)
     todo_list = [problem for problem in problems_list if problem not in done_list]
     return todo_list
 
-new_todo_list = make_todo_list()
+if __name__ == "__main__":
+    #-----------------change params here only-----------------
+    new_todo_list = make_todo_list(is_basic = True)
+    with open('todo.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(["Problem_Name", "Max_Score", "SI_Link"])
+        writer.writerows(new_todo_list)
 
-with open('todo.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(["Problem_Name", "Max_Score", "SI_Link"])
-    writer.writerows(new_todo_list)
--- shell_scripts/temp.sh
+++ shell_scripts/temp.sh
@@ -2,3 +2,9 @@ wmctrl -a meta && sleep 0.1 && xdotool key g key i && sleep 0.1 && xdotool key -
 wmctrl -a meta && sleep 0.1 && xdotool key g+i && sleep 0.1 && xdotool key --repeat 2 BackSpace && xdotool key ctrl+v
 xclip -selection clipboard -f < /path/to/file && xdotool key ctrl+v
 
+wmctrl -a thehive && sleep 0.1 && xdotool key --delay 50 f+s+a
+wmctrl -a thehive && sleep 0.1 && xdotool key Ctrl+s
+wmctrl -a all && sleep 0.1 && xdotool type "yo" && xdotool key Return
+
+
+
